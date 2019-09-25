b2ae9911a30ce92907eb2e044742838f3016cf35
Felipe <felipebetancourt714@gmail.com>
2019-09-25 11:41:51 -0500

    cree activity_fragmento.xml, fragmento.java, carpeta menu en res, menu.mail.xml, en build.garde.implement matarial, activity_menu_mail xml y java, fragmento funcionando, agregue el fondo de menu inicio y perfil

---
+++ app/src/main/java/com/juan/firebaseregistro/Favoritos.java
@@ -0,0 +1,30 @@
+package com.juan.firebaseregistro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Favoritos extends Fragment {
+
+    public Favoritos() {
+
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,@Nullable Bundle savedInstanceState ) {
+
+
+        View view = inflater.inflate(R.layout.activity_favoritos,container,false);
+        return view;
+    }
+}
