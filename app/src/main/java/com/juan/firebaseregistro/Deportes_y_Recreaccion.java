6275f1dd9962e7d0b910c03c466909ec28e6e4e9
Felipe <felipebetancourt714@gmail.com>
2019-09-29 13:48:10 -0500

    agrege fragmente no funcionales de deportes y recreaccion, oferta educativa, oferta laboral

---
+++ app/src/main/java/com/juan/firebaseregistro/Deportes_y_Recreaccion.java
@@ -0,0 +1,14 @@
+package com.juan.firebaseregistro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Deportes_y_Recreaccion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deportes_y_recreaccion);
+    }
+}
