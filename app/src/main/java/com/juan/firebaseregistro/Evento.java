528549a2964caad62302582d8aedb8ab4f39c4a0
juan david <mjuan@unicauca.edu.co>
2019-09-30 13:09:26 -0500

    listar eventos

---
+++ app/src/main/java/com/juan/firebaseregistro/Evento.java
@@ -0,0 +1,105 @@
+package com.juan.firebaseregistro;
+
+public class Evento {
+
+    String nombre;
+    String telefono;
+    String urlImagen;
+
+    public Evento(String nombre, String telefono, String urlImagen, String categoria, String descripcion, String direccion, String fecha, long latitud, long longitud) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.urlImagen = urlImagen;
+        this.categoria = categoria;
+        this.descripcion = descripcion;
+        this.direccion = direccion;
+        this.fecha = fecha;
+        this.latitud = latitud;
+        this.longitud = longitud;
+    }
+
+    public Evento() {
+    }
+
+    String categoria;
+    String descripcion;
+    String direccion;
+    String fecha;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getUrlImagen() {
+        return urlImagen;
+    }
+
+    public void setUrlImagen(String urlImagen) {
+        this.urlImagen = urlImagen;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public long getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(long latitud) {
+        this.latitud = latitud;
+    }
+
+    public long getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(long longitud) {
+        this.longitud = longitud;
+    }
+
+    long latitud, longitud;
+
+
+
+}
--- app/src/main/java/com/juan/firebaseregistro/Menu_Principal.java
+++ app/src/main/java/com/juan/firebaseregistro/Menu_Principal.java
@@ -4,11 +4,31 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Menu_Principal extends Fragment {
+
+    RecyclerView recyclerView;
+    AdaptadorEventos adaptadorEventos;
+    Evento objEvent;
+    List<Evento> itemEventos;
+    private DatabaseReference mDatabase;// ...
 
     public Menu_Principal()
     {
@@ -21,7 +41,49 @@ public class Menu_Principal extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,@Nullable Bundle savedInstanceState ) {
 
 
-        View view = inflater.inflate(R.layout.activity_menu_principal,container,false);
+        final View view = getLayoutInflater().inflate(R.layout.activity_menu_principal,container,false);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        recyclerView= (RecyclerView) view.findViewById(R.id.recycEventosTodos);
+        itemEventos= new ArrayList<>();
+        GridLayoutManager gridLayoutManager= new GridLayoutManager(getContext(),2);
+        recyclerView.setLayoutManager(gridLayoutManager);
+
+
+            mDatabase.child("Evento").addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.exists()) {
+                        for(int i=0; i<dataSnapshot.getChildrenCount();i++) {
+
+                            objEvent = new Evento();
+                            objEvent.setNombre(dataSnapshot.child(""+i).child("nombre").getValue().toString());
+                            objEvent.setUrlImagen(dataSnapshot.child(""+i).child("urlImagen").getValue().toString());
+                            itemEventos.add(objEvent);
+                        }
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+        
+
+        adaptadorEventos= new AdaptadorEventos(itemEventos, getContext(), new AdaptadorEventos.OnItemClick(){
+            @Override
+            public void itemClick(Evento items, int position) {
+
+
+            }
+        });
+        recyclerView.setAdapter(adaptadorEventos);
+
         return view;
     }
-}
+
+
+    }
+
+
+
