4b92df93d2764e391026e82086bc08715ca84aae
DaniloManquillo <54942318+DaniloManquillo@users.noreply.github.com>
2019-10-15 16:28:03 -0500

    agregue la funcion llamar

--- app/src/main/java/com/juan/firebaseregistro/Favoritos.java
+++ app/src/main/java/com/juan/firebaseregistro/Favoritos.java
@@ -2,25 +2,38 @@ package com.juan.firebaseregistro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+
+import java.util.Calendar;
 
 public class Favoritos extends Fragment {
 
 
     TextView nombreevento, descripcionevento, numeroTelefono, direccionEvento;
     ImageView imagenevento;
+    ImageButton botonLlamar, botonCalendario;
+    String telefono;
 
 
     public Favoritos() {
@@ -41,6 +54,8 @@ public class Favoritos extends Fragment {
         descripcionevento =(TextView) view.findViewById(R.id.txt_Descripcion_Del_Evento);
         direccionEvento =(TextView) view.findViewById(R.id.txt_Dirreccion);
         numeroTelefono =(TextView) view.findViewById(R.id.txt_Numero_Telefonico);
+        botonLlamar =(ImageButton) view.findViewById(R.id.btn_Llamar);
+        botonCalendario =(ImageButton) view.findViewById(R.id.btn_Calendario);
 
 
         Bundle eventodetall  = getArguments();
@@ -54,8 +69,47 @@ public class Favoritos extends Fragment {
             descripcionevento.setText(eventomues.getDescripcion());
             direccionEvento.setText(eventomues.getDireccion());
             numeroTelefono.setText(eventomues.getTelefono());
+            telefono = eventomues.getTelefono();
+            final String fecha = eventomues.getFecha();
 
+            botonLlamar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getContext(), "presiono", Toast.LENGTH_SHORT).show();
 
+                    Uri uri = Uri.parse("tel:3122826217");
+                    Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+                    startActivity(intent);
+
+                    /*if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.CALL_PHONE)!=
+                            PackageManager.PERMISSION_GRANTED)
+                            return;*/
+                   
+
+                }
+            });
+
+           /*  botonCalendario.setOnClickListener(new View.OnClickListener() {
+                 @Override
+                 public void onClick(View v) {
+
+                     Calendar cal = Calendar.getInstance();
+                     boolean val = false;
+                     Intent intento = null;
+                     while (val = false)
+                     {
+                         try
+                         {
+
+                             cal.set(Calendar.DATE, fecha);
+
+                         }catch (Exception e)
+                         {
+
+                         }
+                     }
+                 }
+             });*/
         }
 
 
