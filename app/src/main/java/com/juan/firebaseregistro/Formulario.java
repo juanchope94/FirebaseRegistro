fe0a71e5798b93b9d3c735dd594de452fa1fea20
DaniloManquillo <54942318+DaniloManquillo@users.noreply.github.com>
2019-11-22 14:29:12 -0500

    agregado url

--- app/src/main/java/com/juan/firebaseregistro/Formulario.java
+++ app/src/main/java/com/juan/firebaseregistro/Formulario.java
@@ -20,7 +20,7 @@ private WebView m;
         WebSettings webSettings =m.getSettings();
         webSettings.setJavaScriptEnabled(true);
         m.setWebViewClient(new WebViewClient());
-        m.loadUrl("https://docs.google.com/forms/d/e/1FAIpQLSe2oFb5h08j_6UHmSvMjWe48Vbx-u628cYMEUC6fPdcq38t_g/viewform?usp=sf_link/");
+        m.loadUrl(Favoritos.urlformularioo);
 
     }
 }
