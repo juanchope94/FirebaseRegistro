642e3e8fb4e28502af41b633befb44e95045f5e2
juan david <mjuan@unicauca.edu.co>
2019-11-29 16:27:11 -0500

    seccion de me interesa agregada nuevamente

--- app/src/main/java/com/juan/firebaseregistro/Favoritos.java
+++ app/src/main/java/com/juan/firebaseregistro/Favoritos.java
@@ -9,12 +9,17 @@ import android.net.Uri;
 import android.nfc.Tag;
 import android.os.Build;
 import android.os.Bundle;
-
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import java.util.HashMap;
+import java.util.Map;
 
 import android.provider.CalendarContract;
 import android.util.Log;
@@ -31,6 +36,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.nightonke.boommenu.BoomMenuButton;
 import com.nightonke.boommenu.Types.BoomType;
@@ -48,7 +54,7 @@ public class Favoritos extends Fragment {
 
     TextView nombreevento, descripcionevento, numeroTelefono, direccionEvento, fechaEvento;
     ImageView imagenevento;
-    ImageButton botonLlamar, botonCalendario, botonubicacion;
+    ImageButton botonLlamar, botonCalendario, botonubicacion, btn_interes;;
     Button incripcion;
     String telefono;
     String titulo, descripcion;
@@ -84,11 +90,19 @@ public class Favoritos extends Fragment {
         numeroTelefono =(TextView) view.findViewById(R.id.txt_Numero_Telefonico);
         botonLlamar =(ImageButton) view.findViewById(R.id.btn_Llamar);
         botonCalendario =(ImageButton) view.findViewById(R.id.btn_Calendario);
-incripcion = (Button)view.findViewById(R.id.btnIncripcion);
+        incripcion = (Button)view.findViewById(R.id.btnIncripcion);
         fechaEvento =(TextView) view.findViewById(R.id.txt_Fecha_Del_Evento);
         botonubicacion =(ImageButton) view.findViewById(R.id.btn_Ubicacion);
-     //   boomMenuButton =(BoomMenuButton) view.findViewById(R.id.idboom);
+        btn_interes =(ImageButton)  view.findViewById(R.id.btn_Me_Interesa);
 
+        //   boomMenuButton =(BoomMenuButton) view.findViewById(R.id.idboom);
+
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            btn_interes.setVisibility(view.VISIBLE);
+        }
+        else {
+            btn_interes.setVisibility(view.INVISIBLE);
+        }
 
         Bundle eventodetall  = getArguments();
 
@@ -111,6 +125,8 @@ incripcion = (Button)view.findViewById(R.id.btnIncripcion);
             longitud = eventomues.getLongitud();
             urlformularioo = eventomues.getUrlInscripcion();
 
+
+            final String id= eventomues.getId();
             final String fecha = eventomues.getFecha();
 
             String[] parts = fecha.split("-");
@@ -123,6 +139,33 @@ incripcion = (Button)view.findViewById(R.id.btnIncripcion);
                 public void onClick(View v) {
                     Intent formular = new Intent(getContext(), Formulario.class);
                     startActivity(formular);
+                }
+            });
+
+            btn_interes.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Map<String,Object> map = new HashMap<>();
+                    map.put("idEvento",id);
+                    map.put("correo", FirebaseAuth.getInstance().getCurrentUser().getEmail());
+                    map.put("nombre", FirebaseAuth.getInstance().getCurrentUser().getDisplayName());
+                    FirebaseFirestore db = FirebaseFirestore.getInstance();
+                    db.collection("Interes")
+                            .add(map)
+                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                @Override
+                                public void onSuccess(DocumentReference documentReference) {
+
+                                    Toast.makeText(getContext(), "Te intereso este evento!", Toast.LENGTH_LONG).show();
+                                }
+                            })
+
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+
+                                }
+                            });
                 }
             });
 
@@ -200,67 +243,11 @@ incripcion = (Button)view.findViewById(R.id.btnIncripcion);
 
 
 
-        return view;
+                         return view;
 
 
 
-
-    }
-
-/*
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-
-
-
-        int [][]  suButtonColors = new int[3][2];
-        for (int i = 0 ; i<3;i++ )
-
-        {
-
-            suButtonColors[i][1] = ContextCompat.getColor(getContext(), R.color.Azul_Botones);
-            suButtonColors[i][0] = Util.getInstance().getPressedColor(suButtonColors[i][1]);
-
-        }
-
-        new BoomMenuButton.Builder()
-                .addSubButton (ContextCompat.getDrawable(getContext(),R.drawable.ic_gps),suButtonColors[0],"Menu Boton")
-                .addSubButton (ContextCompat.getDrawable(getContext(),R.drawable.ic_bienestar),suButtonColors[0],"Recurso de Codigo")
-
-
-                .button(ButtonType.CIRCLE)
-                .boom(BoomType.LINE)
-                .place(PlaceType.CIRCLE_8_1)
-                .subButtonTextColor(ContextCompat.getColor(getContext(),R.color.default_boom_button_color))
-                .subButtonsShadow(Util.getInstance().dp2px(2),Util.getInstance().dp2px(2))
-                .onSubButtonClick(new BoomMenuButton.OnSubButtonClickListener() {
-                                      @Override
-                                      public void onClick(int buttonIndex) {
-
-                                          switch (buttonIndex){
-
-                                              case 0:
-                                                  Toast.makeText(getContext(), "Primer boton", Toast.LENGTH_SHORT).show();
-                                                  break;
-
-                                              case 1:
-                                                  Toast.makeText(getContext(), "segundo boton", Toast.LENGTH_SHORT).show();
-                                                  break;
-
-
-
-
-                                          }
-                                      }
-                                  }
-
-                )
-                .init(boomMenuButton);
-
-
-
-    }*/
-
+                }
 
     }
 
